cf6300c1019dfd6508e66d70b4be7f4c96e1f201
Pierpaolo Calanna <p.calanna@gmail.com>
2025-12-29 10:55:33 +0100

    Add function to get DataFrame column indices by regex
    Introduced get_columns_indices_from_regex to return indices of DataFrame columns matching a regex pattern. Also added necessary numpy and pandas imports.

--- lib/utils.py
+++ lib/utils.py
@@ -1,3 +1,8 @@
+import numpy as np
+import pandas as pd
+from numpy.typing import NDArray
+
+
 def greet(name: str) -> str:
     """Just a simple greeting function.
 
@@ -9,3 +14,20 @@ def greet(name: str) -> str:
 
     """
     return f"Hello, {name}!"
+
+
+def get_columns_indices_from_regex(df: pd.DataFrame, regex: str) ->NDArray[np.int_]:
+    """Get the indices of columns in a DataFrame that match a given regex pattern.
+    
+    Args:
+        df (pd.DataFrame): The DataFrame to search.
+        regex (str): The regex pattern to match column names.
+    
+    Returns:
+        NDArray[np.int_]: An array of indices of matching columns.
+    """
+    # Create a boolean mask where column names match the regex
+    condition: pd.Series = df.columns.str.contains(regex, regex=True)
+    
+    # Return the indices of the columns that match the regex
+    return np.asarray(condition).nonzero()[0]
