c3a846be076556773873bb962f229df8a3bbfc0c
Pierpaolo Calanna <p.calanna@gmail.com>
2025-12-29 10:16:03 +0100

    Add initial paths and utils modules
    Introduced lib/paths.py with a DATAPATH constant using pathlib, and lib/utils.py with a simple greet function.

---
+++ lib/utils.py
@@ -0,0 +1,11 @@
+def greet(name: str) -> str:
+    """Just a simple greeting function.
+
+    Args:
+        name (str): The name of the person to greet.
+
+    Returns:
+        str: A greeting message.
+
+    """
+    return f"Hello, {name}!"
