3ed31aacfa282451803dab17c718c57bcb146456
Pierpaolo Calanna <p.calanna@gmail.com>
2026-01-05 18:30:06 +0100

    Update __init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,9 +1,4 @@
-import warnings
-
 import matplotlib.pyplot as plt
-
-# Set up warnings filter
-warnings.filterwarnings("ignore")
 
 # Set matplotlib font to sans-serif
 plt.rcParams["font.family"] = "sans-serif"
